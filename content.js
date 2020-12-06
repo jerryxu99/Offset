c1edfa99a8cc0b6bc7ed998e961eacece3208abe
Jerry Xu <jerry.xu99@gmail.com>
2020-12-06 14:12:50 -0800

    parse checkout value

--- content.js
+++ content.js
@@ -1,1 +1,13 @@
-alert('Hello from your Chrome extension!');
+// var t = document.querySelector('table'),
+//   tr = t.querySelector('.total');
+// td = tr.getElementsByTagName('td')[0];
+// console.log(tr);
+// console.log(td);
+
+var priceString = document
+  .getElementById('sc-subtotal-amount-buybox')
+  .querySelector('span')
+  .innerHTML.substring(10);
+
+var price = parseFloat(priceString);
+console.log(price);
