9ddd4f3562481dfc353430915f7fb322da7bba65
Jerry Xu <jerry.xu99@gmail.com>
2020-12-06 14:28:40 -0800

    added png

--- content.js
+++ content.js
@@ -1,9 +1,3 @@
-// var t = document.querySelector('table'),
-//   tr = t.querySelector('.total');
-// td = tr.getElementsByTagName('td')[0];
-// console.log(tr);
-// console.log(td);
-
 var priceString = document
   .getElementById('sc-subtotal-amount-buybox')
   .querySelector('span')
