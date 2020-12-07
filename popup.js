879f781055ccfc4dcfa7c99317c1ee534f83813b
Jerry Xu <jerry.xu99@gmail.com>
2020-12-06 17:04:37 -0800

    format price

--- popup.js
+++ popup.js
@@ -1,12 +1,17 @@
 document.getElementById('donate').addEventListener('click', () => {
   var win = window.open('https://carbonfund.org/donate', '_blank');
   win.focus();
+  var price = document.querySelector('#price').innerHTML;
+  price.select();
+  document.execCommand('copy');
 });
 
 document.addEventListener('DOMContentLoaded', () => {
   chrome.storage.local.get('price', (response) => {
     if (response.price) {
-      document.getElementById('price').innerHTML = response.price;
+      document.getElementById('price').innerHTML = (
+        response.price * 0.05
+      ).toFixed(2);
     }
   });
 });
