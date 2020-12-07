2a9e99e1cac7e22c40b4570a1b9086bf003c6482
malcolm <malcolmmackenzie20@gmail.com>
2020-12-06 17:03:15 -0800

    moved $ to same line

--- popup.js
+++ popup.js
@@ -6,7 +6,7 @@ document.getElementById('donate').addEventListener('click', () => {
 document.addEventListener('DOMContentLoaded', () => {
   chrome.storage.local.get('price', (response) => {
     if (response.price) {
-      document.getElementById('price').innerHTML = response.price;
+      document.getElementById('price').innerHTML = '$' + ((response.price*0.05).toFixed(2)).toString();
     }
   });
 });
